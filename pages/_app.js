145f963cfc7b4a43c4a2bee27538aad15608a44c
vinismachadoo <vinicius.sanches@poli.ufrj.br>
2021-11-30 20:45:42 -0300

    scroll bar styling w tlw

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,13 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="overscroll-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-white hover:scrollbar-thumb-gray-100 scrollbar-track-transparent">
       <Head>
         <title>EmojiArk NFT</title>
         <link rel="icon" href="/icon.png" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
