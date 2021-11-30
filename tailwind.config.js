c689764d1146d8682179b817ed058137fadfbaa2
vinismachadoo <vinicius.sanches@poli.ufrj.br>
2021-11-30 15:09:37 -0300

    scrollbar plugin style tlw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
+    scrollbar: ["rounded"],
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
